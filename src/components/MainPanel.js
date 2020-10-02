36a4a60a5ff473d9b307d521744e602b7ac8451b
anthopark <kunhpark15@gmail.com>
2020-10-01 18:27:36 -0700

    started redesigning and refactoring project

---
+++ src/components/MainPanel.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+
+
+`;
+
+class MainPanel extends Component {
+    render() {
+        return (
+            <Container>
+            
+            </Container>
+        );
+    }
+}
+
+export default MainPanel;
---
+++ src/components/SidePanel.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+
+
+`;
+
+class SidePanel extends Component {
+    render() {
+        return (
+            <Container>
+            
+            </Container>
+        );
+    }
+}
+
+export default SidePanel;
