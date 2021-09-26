115c6c1518379130eb0de2d2b8e4c512f02a87b6
Anthony Park <kunhpark15@gmail.com>
2021-09-26 18:18:14 -0400

    add prettier to nextjs project

--- nextjs-client/src/pages/_app.js
+++ nextjs-client/src/pages/_app.js
@@ -1,7 +1,7 @@
-import '../src/styles/globals.css'
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
