4fd90896e838aa4629b8298176041620a3867ea5
Anthony Park <kunhpark15@gmail.com>
2021-09-26 17:39:21 -0400

    add eslint to nextjs project

--- nextjs-client/src/pages/index.js
+++ nextjs-client/src/pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../src/styles/Home.module.css'
 
+
 export default function Home() {
   return (
     <div className={styles.container}>
