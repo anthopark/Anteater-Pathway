87ec3a6bc3e04fc9035520dd421b91878ac7ff58
Emily <pheepia7788@gmail.com>
2022-12-06 15:49:47 -0800

    add temp theme toggler btn

--- new-nextjs-client/pages/index.tsx
+++ new-nextjs-client/pages/index.tsx
@@ -1,9 +1,10 @@
 import styles from './index.module.scss';
 import MainLayout from '@components/MainLayout/MainLayout';
 import { ReactNode } from 'react';
+import ThemeToggler from '@components/ThemeToggler/ThemeToggler';
 
 export default function Home() {
-  return <></>;
+  return <ThemeToggler />;
 }
 
 Home.getLayout = function getLayout(page: ReactNode) {
