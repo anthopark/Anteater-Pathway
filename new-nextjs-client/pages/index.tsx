37e9cff2acc2456734efe2eb560db8e6eaa37dfb
Emily <pheepia7788@gmail.com>
2022-12-06 18:32:49 -0800

    add color variables and update left side bar background color

--- new-nextjs-client/pages/index.tsx
+++ new-nextjs-client/pages/index.tsx
@@ -4,7 +4,12 @@ import { ReactNode } from 'react';
 import ThemeToggler from '@components/ThemeToggler/ThemeToggler';
 
 export default function Home() {
-  return <ThemeToggler />;
+  return (
+    <div className="home-body">
+      <ThemeToggler />
+      <div>HI</div>
+    </div>
+  );
 }
 
 Home.getLayout = function getLayout(page: ReactNode) {
