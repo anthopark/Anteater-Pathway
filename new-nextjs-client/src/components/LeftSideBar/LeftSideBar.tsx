e5fdbfa5f3130b937b55be49ca4138c94947e46b
Emily <pheepia7788@gmail.com>
2022-12-21 17:52:30 -0800

    add left side bar style and main logo

--- new-nextjs-client/src/components/LeftSideBar/LeftSideBar.tsx
+++ new-nextjs-client/src/components/LeftSideBar/LeftSideBar.tsx
@@ -3,6 +3,7 @@ import style from './LeftSideBar.module.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import { findIconDefinition } from '@fortawesome/fontawesome-svg-core';
+import Image from 'next/image';
 
 const coffee = findIconDefinition({ prefix: 'fas', iconName: 'coffee' });
 const envelope = findIconDefinition({ prefix: 'fas', iconName: 'envelope' });
@@ -12,15 +13,22 @@ export interface ILeftSideBarProps {}
 export default function LeftSideBar(props: ILeftSideBarProps) {
   return (
     <div className={style.container}>
-      <div className="menu-container">
-        <a>
-          <FontAwesomeIcon icon={coffee} color="white" />
+      <div className={style.logoContainer}>
+        <Image src="/logo.svg" alt="logo" layout="fill" />
+      </div>
+      <div className={style.menuContainer}>
+        <a className={style.link}>
+          <FontAwesomeIcon
+            className={style.icon}
+            icon={envelope}
+            color="white"
+          />
         </a>
-        <a>
-          <FontAwesomeIcon icon={envelope} color="white" />
+        <a className={style.link}>
+          <FontAwesomeIcon className={style.icon} icon={github} color="white" />
         </a>
-        <a>
-          <FontAwesomeIcon icon={github} color="white" />
+        <a className={style.link}>
+          <FontAwesomeIcon className={style.icon} icon={coffee} color="white" />
         </a>
       </div>
     </div>
