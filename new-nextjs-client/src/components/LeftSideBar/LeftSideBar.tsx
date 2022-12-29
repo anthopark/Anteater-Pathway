b7ce3dbcc1324926fe5c865a574ff51600c65587
Anthony Park <kunhpark15@gmail.com>
2022-12-28 20:54:46 -0800

    fix hydration issue for useTheme

--- new-nextjs-client/src/components/LeftSideBar/LeftSideBar.tsx
+++ new-nextjs-client/src/components/LeftSideBar/LeftSideBar.tsx
@@ -1,4 +1,5 @@
 import style from './LeftSideBar.module.scss';
+import { useState, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { coffee, envelope, github } from '@styles/fontawesome';
 import Image from 'next/image';
@@ -7,15 +8,24 @@ import { useTheme } from 'next-themes';
 export interface ILeftSideBarProps {}
 
 export default function LeftSideBar(props: ILeftSideBarProps) {
-  const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+  const { theme } = useTheme();
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null;
+  }
 
   return (
     <div className={style.container}>
       <div className={style.logoContainer}>
         {theme === 'light' ? (
-          <Image src="/logo-light.svg" alt="logo" layout="fill" />
+          <Image src="/logo-light.svg" alt="logo" fill />
         ) : (
-          <Image src="/logo-dark.svg" alt="logo" layout="fill" />
+          <Image src="/logo-dark.svg" alt="logo" fill />
         )}
       </div>
       <div className={style.menuContainer}>
