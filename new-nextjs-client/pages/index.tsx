68f916c762466b375469e5ecfa8b2b42cfa0db3d
Emily <pheepia7788@gmail.com>
2023-01-08 14:19:26 -0800

    remove code out of scope

--- new-nextjs-client/pages/index.tsx
+++ new-nextjs-client/pages/index.tsx
@@ -3,28 +3,19 @@ import MainLayout from '@components/MainLayout/MainLayout';
 import { ReactNode } from 'react';
 import ThemeToggler from '@components/ThemeToggler/ThemeToggler';
 import AddYearDropdown from '@components/IndexPage/AddYearDropdown/AddYearDropdown';
-import AppButton from '@components/shared/AppButton/AppButton';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { search } from '@styles/fontawesome';
-import { white1 } from '@styles/variables';
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <div className={styles.topLayout}>
-        <div className={styles.addYear}>
-          <AddYearDropdown />
+        <div className={styles.leftContainer}>
+          <div className={styles.addYear}>
+            <AddYearDropdown />
+          </div>
         </div>
-        <AppButton
-          onClick={(e) => console.log(e)}
-          kind="primary"
-          leftIcon={<FontAwesomeIcon icon={search} color={white1} size="1x" />}
-        >
-          Search Courses
-        </AppButton>
-
-        <ThemeToggler />
-        <div className={styles.userProfile}></div>
+        <div className={styles.rightContainer}>
+          <ThemeToggler />
+        </div>
       </div>
 
       <div className={styles.mainLayout}></div>
