76bbcf0ae13fd537b9275b0fe72c51ad0f698e74
Emily <pheepia7788@gmail.com>
2023-01-08 20:25:53 -0800

    remove IconButton container

--- new-nextjs-client/src/components/ThemeToggler/ThemeToggler.tsx
+++ new-nextjs-client/src/components/ThemeToggler/ThemeToggler.tsx
@@ -1,15 +1,12 @@
 import { useState, useEffect } from 'react';
 import { useTheme } from 'next-themes';
 import { IconButton } from '@chakra-ui/react';
-import { controlHeightMD } from '@styles/variables';
 import { sun, moon } from '@styles/fontawesome';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import styles from './ThemeToggler.module.scss';
 
-type Props = {};
-
-export default function ThemeToggler({}: Props) {
+function ThemeToggler() {
   const [mounted, setMounted] = useState(false);
   const { theme, setTheme } = useTheme();
 
@@ -22,21 +19,21 @@ export default function ThemeToggler({}: Props) {
   }
 
   return (
-    <div className={styles.container}>
-      <IconButton
-        height={'3rem'}
-        width={'3rem'}
-        onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
-        aria-label="Theme toggle"
-        variant="unstyled"
-        icon={
-          theme === 'light' ? (
-            <FontAwesomeIcon className={styles.themeIcon} icon={moon} />
-          ) : (
-            <FontAwesomeIcon className={styles.themeIcon} icon={sun} />
-          )
-        }
-      />
-    </div>
+    <IconButton
+      height={'3rem'}
+      width={'3rem'}
+      onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
+      aria-label="Theme toggle"
+      variant="unstyled"
+      icon={
+        theme === 'light' ? (
+          <FontAwesomeIcon className={styles.themeIcon} icon={moon} />
+        ) : (
+          <FontAwesomeIcon className={styles.themeIcon} icon={sun} />
+        )
+      }
+    />
   );
 }
+
+export default ThemeToggler;
