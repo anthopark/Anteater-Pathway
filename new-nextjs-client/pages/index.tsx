d777eb93078e22917ba82aa2696b5eeb3c038c9e
Emily <pheepia7788@gmail.com>
2023-01-13 17:33:48 -0800

    add courses button

--- new-nextjs-client/pages/index.tsx
+++ new-nextjs-client/pages/index.tsx
@@ -4,6 +4,9 @@ import { ReactNode } from 'react';
 import ThemeToggler from '@components/ThemeToggler/ThemeToggler';
 import AddYearDropdown from '@components/index-page/AddYearDropdown/AddYearDropdown';
 import Avatar from '@components/index-page/Avatar/Avatar';
+import AppButton from '@components/shared/AppButton/AppButton';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { search } from '@styles/fontawesome';
 
 export default function Home() {
   return (
@@ -12,6 +15,14 @@ export default function Home() {
         <div className={styles.leftContainer}>
           <div className={styles.addYearDropdownWrapper}>
             <AddYearDropdown />
+          </div>
+          <div className={styles.searchBtnWrapper}>
+            <AppButton
+              kind="primary"
+              leftIcon={<FontAwesomeIcon icon={search} />}
+            >
+              Courses
+            </AppButton>
           </div>
         </div>
         <div className={styles.rightContainer}>
