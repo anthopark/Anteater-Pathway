73ca095854b7f83219a771ab3cb44635a41c471b
Anthony Park <kunhpark15@gmail.com>
2023-02-02 07:06:14 -0800

    menu trigger works with drag n drop

--- new-nextjs-client/src/components/index-page/SortableCourseItem/SortableCourseItem.tsx
+++ new-nextjs-client/src/components/index-page/SortableCourseItem/SortableCourseItem.tsx
@@ -4,9 +4,10 @@ import classNames from 'classnames/bind';
 import { useSortable } from '@dnd-kit/sortable';
 import { CSS } from '@dnd-kit/utilities';
 import { UniqueIdentifier } from '@dnd-kit/core';
-import { useState } from 'react';
+import { CSSProperties, MouseEventHandler, useEffect, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { threeDot } from '@styles/fontawesome';
+import { Menu, MenuButton, MenuItem, MenuList } from '@chakra-ui/react';
 
 interface Props {
   course?: Course;
@@ -26,11 +27,21 @@ const SortableCourseItem = (props: Props) => {
     isDragging,
   } = useSortable({ id: props.sortableId ?? props.course!.id });
   const [isHover, setIsHover] = useState(false);
+  const [isMenuTriggerHover, setIsMenuTriggerHover] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-  const style = {
+  const style: CSSProperties = {
     transform: CSS.Transform.toString(transform),
     transition,
   };
+
+  useEffect(() => {
+    if (isDragging) {
+      setIsMenuOpen(false);
+      setIsHover(false);
+      setIsMenuTriggerHover(false);
+    }
+  }, [isDragging]);
 
   const deptCode = () => {
     if (props.sortableId) {
@@ -46,49 +57,88 @@ const SortableCourseItem = (props: Props) => {
     return props.course!.courseInfo.num;
   };
 
-  const handleMouseEnter = () => {
+  const handleContainerMouseEnter = () => {
     if (Boolean(props.sortableId)) {
       return setIsHover(false);
     }
     setIsHover(true);
   };
 
-  const handleMouseLeave = () => {
+  const handleContainerMouseLeave = () => {
     setIsHover(false);
+  };
+
+  const handleMenuTriggerMouseEnter = () => {
+    setIsMenuTriggerHover(true);
+  };
+
+  const handleMenuTriggerMouseLeave = () => {
+    setIsMenuTriggerHover(false);
   };
 
   return (
     <div
-      {...attributes}
-      {...listeners}
-      className={cx('container', {
-        dragging: isDragging,
-        'drag-overlay': Boolean(props.sortableId),
-      })}
-      ref={setNodeRef}
-      style={style}
-      onMouseEnter={handleMouseEnter}
-      onMouseLeave={handleMouseLeave}
+      className={cx('outer-container')}
+      onMouseEnter={handleContainerMouseEnter}
+      onMouseLeave={handleContainerMouseLeave}
     >
-      {isHover ? (
+      <div
+        style={style}
+        ref={setNodeRef}
+        className={cx('container', {
+          dragging: isDragging,
+          'drag-overlay': Boolean(props.sortableId),
+        })}
+      >
+        <div className={cx('dept-code-num')}>
+          <div className={cx('dept-code')}>{deptCode()}</div>
+          <div className={cx('num')}>{num()}</div>
+        </div>
+      </div>
+      {isHover || isMenuOpen ? (
         <>
-          <div className={cx('overlay')}></div>
           <div
-            className={cx('menu-trigger')}
-            onClick={(e) => {
-              e.preventDefault();
-              e.stopPropagation();
+            className={cx('overlay')}
+            style={{ cursor: 'grab' }}
+            {...attributes}
+            {...listeners}
+          ></div>
+          <Menu
+            strategy={'fixed'}
+            onOpen={() => setIsMenuOpen(true)}
+            onClose={() => {
+              setIsMenuOpen(false);
+              setIsMenuTriggerHover(false);
             }}
           >
-            <FontAwesomeIcon className={cx('three-dot-icon')} icon={threeDot} />
-          </div>
+            <MenuButton
+              position="absolute"
+              w="1.5rem"
+              h="2.5rem"
+              top="0.65rem"
+              right="0.5rem"
+              onMouseEnter={handleMenuTriggerMouseEnter}
+              onMouseLeave={handleMenuTriggerMouseLeave}
+            >
+              <div
+                className={cx('menu-trigger', {
+                  'menu-trigger-hover': isMenuTriggerHover,
+                  'menu-open': isMenuOpen,
+                })}
+              >
+                <FontAwesomeIcon
+                  className={cx('three-dot-icon', {
+                    'menu-trigger-hover': isMenuTriggerHover,
+                    'menu-open': isMenuOpen,
+                  })}
+                  icon={threeDot}
+                />
+              </div>
+            </MenuButton>
+            <MenuList></MenuList>
+          </Menu>
         </>
       ) : null}
-
-      <div className={cx('dept-code-num')}>
-        <div className={cx('dept-code')}>{deptCode()}</div>
-        <div className={cx('num')}>{num()}</div>
-      </div>
     </div>
   );
 };
